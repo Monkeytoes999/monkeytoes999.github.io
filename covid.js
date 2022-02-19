c93ee296b27921a2a8e9c7c841e55558804ebb89
Monkeytoes999 <graytokarz@yahoo.com>
2022-02-19 17:45:21 -0500

    Commented code

--- covid.js
+++ covid.js
@@ -1,14 +1,26 @@
-
+/**
+ * Queries a current Covid API and returns current data 
+ * @returns a JSON file that holds current Covid data
+ */
 async function callCovidAPI() {
     let response = await fetch("https://api.covidactnow.org/v2/country/US.json?apiKey=dd1370169b8c4397b1ffc66b629b03da");
     let data = await response.json()
     return data;
 }
 
+/**
+ * Procresses the JSON data to find the relevant current data
+ * @returns Information about the current covid data in the US
+ */
 async function covidJson() {
     return await callCovidAPI().then(data => {return changeHtml("<h3>United States Daily Covid Report</h3>\nDaily reported cases: " + data["actuals"]["newCases"] + "\nDaily reported deaths: " + data["actuals"]["newDeaths"] + "\nLast updated: " + data["lastUpdatedDate"])});
 }
 
+/**
+ * Creates HTML data that will display the Covid data in a legible way
+ * @param {Covid data} content 
+ * @returns a HTML widget
+ */
 function changeHtml(content) {
     var par = content.split("\n")
     var base = document.createElement("div");
--- jokes.js
+++ jokes.js
@@ -1,13 +1,27 @@
+/**
+ * Queries a dad joke API and returns a random joke
+ * @returns a JSON structure that holds a dad joke
+ */
 async function callDadJokesAPI() {
     let response = await fetch("https://icanhazdadjoke.com/slack");
     let data = await response.json();
     return data;
 }
 
+/**
+ * Processes API data to extract the dad joke
+ * @returns the dad joke
+ */
 async function dadJokesJson() {
     return await callDadJokesAPI().then(data => {return changeDadHtml(data["attachments"]["0"]["text"])});
 }
 
+/**
+ * Creates HTML data that will display the dad joke in a readable way
+ * with an option to refresh the displayed joke
+ * @param {Joke} content 
+ * @returns a HTML widget
+ */
 function changeDadHtml(content) {
     var par = content.split("\n");
     var base = document.createElement("div");
