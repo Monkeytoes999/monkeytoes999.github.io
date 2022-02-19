7fff3f5a1dbd971281ba6053c245202ae1f7454f
Bret Jones <bretcj1@gmail.com>
2022-02-19 00:08:49 -0500

    Closable sidebar

--- sidebar.js
+++ sidebar.js
@@ -1,3 +1,8 @@
 function expand() {
-    document.getElementById("sidebar").style.display = "block";
+    var temp = document.getElementById("sidebar").style.display;
+    if (temp == "block") {
+        document.getElementById("sidebar").style.display = "none";
+    } else {
+        document.getElementById("sidebar").style.display = "block";
+    }
 }
