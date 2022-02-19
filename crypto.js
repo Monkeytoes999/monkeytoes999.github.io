18250ea6179c069ce8f2cbd3ff927da0fb7150c5
simondaug <daugherty.simon@gmail.com>
2022-02-19 17:39:15 -0500

    Commented code

--- crypto.js
+++ crypto.js
@@ -1,29 +1,41 @@
+/**
+ * Calls CoinGecko to get lastest prices on crypto currency.
+ * @returns a JSON object containing data on crypto
+ */
 async function callCryptoAPI() {
     let response = await fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd");
     let data = await response.json()
     return data;
-}
+} // callCryptoAPI
 
+/**
+ * Calls the CoinGecko API and parses the JSON it returns.
+ * @returns a string containg bitcoin, ethereum, and dogecoin prices
+ */
 async function cryptoJson() {
     return await callCryptoAPI().then(data => {return changeCryptoHtml("Bitcoin Price: $" + data["0"]["current_price"] + "\n" 
         + "Ethereum Price: $" + data["1"]["current_price"] + "\n"
         + "Dogecoin Price: $" + data["11"]["current_price"] )});
-}
+} // cryptoJson
 
+/**
+ * Creates a crypto widget by parsing a string.
+ * @param {*} content a string containing the prices of various crypto currencies
+ * @returns a crypto widget
+ */
 function changeCryptoHtml(content) {
     var par = content.split("\n")
     var base = document.createElement("div");
     base.classList.add("widget");
-
+    // Creates the widget title
     var head = document.createElement("h3");
     head.innerHTML = "Crypto Prices";
     base.appendChild(head);
-
+    // Loops through the input to separate the different crypto
     for (let i = 0; i < par.length; i++){
         var text = document.createElement("p");
         text.innerHTML = par[i];
         base.appendChild(text);
-    }
-
+    } // for
     return base;
-}
+} // changeCryptoHtml
--- news.js
+++ news.js
@@ -1,25 +1,38 @@
+/**
+ * Calls the Guardian to get the latest news.
+ * @returns a JSON object containing data on the latest news stories
+ */
 async function callNewsAPI() {
     let response = await fetch("https://content.guardianapis.com/search?api-key=b86d1bc0-85fb-44ea-ad0c-7f58e8991ec0");
     let data = await response.json()
     return data;
-}
+} // callNewsAPI
 
+/**
+ * Calls the Guardian API and parses the JSON it returns.
+ * @returns a string containing news story titles and links
+ */
 async function newsJson() {
     return await callNewsAPI().then(data => {return changeNewsHtml(data["response"]["results"]["0"]["webTitle"] + "\n" + data["response"]["results"]["0"]["webUrl"]
         + "\n" + data["response"]["results"]["1"]["webTitle"] + "\n" + data["response"]["results"]["1"]["webUrl"]
         + "\n" + data["response"]["results"]["2"]["webTitle"] + "\n" + data["response"]["results"]["2"]["webUrl"])});
-}
+} // newsJson
 
+/**
+ * Creates a news widget by parsing a string.
+ * @param {*} content a string contating titles and links separated by new line characters
+ * @returns A news widget
+ */
 function changeNewsHtml(content) {
     var par = content.split("\n")
     var base = document.createElement("div");
     base.classList.add("widget");
     base.style.lineHeight = 1.25;
-
+    // Create the title for the widget
     var head = document.createElement("h3");
     head.innerHTML = "The Guardian News";
     base.appendChild(head);
-
+    // Loop through the input to pair up the titles and links
     for (let i = 0; i < par.length; i++){
         var text = document.createElement("a");
         text.innerHTML = par[i];
@@ -28,7 +41,6 @@ function changeNewsHtml(content) {
         base.appendChild(text);
         var lineBreak = document.createElement("br");
         base.appendChild(lineBreak);
-    }
-
+    } // for
     return base;
-}
+} // changeNewsHtml
