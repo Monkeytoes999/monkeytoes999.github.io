f36eabf1d82c44a7f795f310e1cb6c7127cdc801
Bret Jones <bretcj1@gmail.com>
2022-02-19 10:53:55 -0500

    Updated how settings is shown

--- sidebar.js
+++ sidebar.js
@@ -23,10 +23,8 @@ function makeDropdown() {
     return list;
 }
 function expand() {
-    var temp = document.getElementById("sidebar").style.display;
-    if (temp == "block") {
-        document.getElementById("sidebar").style.display = "none";
-    } else {
-        document.getElementById("sidebar").style.display = "block";
-    }
+    document.getElementById("sidebar").style.display = "block";
+}
+function shrink() {
+    document.getElementById("sidebar").style.display = "none";
 }
