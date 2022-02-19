633155f5abd453e7d3dac988b4652ef6663725fd
Monkeytoes999 <graytokarz@yahoo.com>
2022-02-19 00:00:50 -0500

    API Template Reference

--- start.js
+++ start.js
@@ -1,1 +1,11 @@
 addDemo();
+
+async function fetchAPI() {
+    let response = await fetch("api");
+    let data = await response.json()
+    return data;
+}
+
+function apiName() {
+    console.log(fetchTest().then(data => console.log(data)));
+}
