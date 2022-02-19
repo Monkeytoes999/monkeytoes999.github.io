2d03359f9ac62866b043fc3689e23cfb99a039a1
SpyseaRice <spysearice@gmail.com>
2022-02-19 14:26:39 -0500

    Revert "Fixed weather api fetch probably"
    This reverts commit 066e6610b22110f9721e2a1fda17b747a6e57a47.

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 async function callW() {
-    let response = await fetch("api.openweathermap.org/data/2.5/weather?lat=33.957409&lon=-83.376801&units=imperial&appid=0720fc8331f4175cbeb36cf226c9f876");
+    let response = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=33.957409&lon=-83.376801&appid=0720fc8331f4175cbeb36cf226c9f876");
     let data = await response.json()
     return data;
 }
