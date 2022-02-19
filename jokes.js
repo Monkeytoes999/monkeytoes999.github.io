bea535a183b9549dccf461a49f6f4909ea78a773
Monkeytoes999 <graytokarz@yahoo.com>
2022-02-19 15:52:35 -0500

    Dad joke button refresh work yes

--- jokes.js
+++ jokes.js
@@ -25,7 +25,27 @@ function changeDadHtml(content) {
     var button = document.createElement("button");
     button.innerHTML = "New Joke";
     button.onclick = async function() {
-        await dadJokesJson();
+        console.log("cheese")
+        data = await callDadJokesAPI();
+        var par = data["attachments"]["0"]["text"].split("\n")
+        while (base.childElementCount > 1){
+            base.removeChild(base.firstChild);
+        }
+        temp = base.firstChild;
+        base.removeChild(base.firstChild);
+
+        var head = document.createElement("h3");
+        head.innerHTML = "Jokes";
+        base.appendChild(head); 
+
+        for (let i = 0; i < par.length; i++){
+            var text = document.createElement("p");
+            text.innerHTML = par[i];
+            base.appendChild(text);
+        }
+
+        base.appendChild(temp);
+
     }
     base.appendChild(button);
 
