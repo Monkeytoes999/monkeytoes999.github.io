87d22686929fb41a00c2a55af54315edc5d4d67f
Bret Jones <bretcj1@gmail.com>
2022-02-21 10:55:58 -0500

    Start

---
+++ sidebar/sidebar.js
@@ -0,0 +1,20 @@
+var showing = 2;
+function openSettings() {
+    document.getElementById("sidebar").style.display = "block";
+    showing = 1;
+}
+function closeSettings() {
+    document.getElementById("sidebar").style.display = "none";
+    showing = 2;
+}
+window.addEventListener("click", function() {
+    if (showing == 1) {
+        openSettings();
+    } else if (showing > 1) {
+        closeSettings();
+    }
+    showing = showing + 1;
+});
+document.getElementById("settings").addEventListener("click", function() {
+    showing = 1;
+});
