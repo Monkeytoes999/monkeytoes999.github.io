3890c4f944dfa3fca8c3ae6def90a603b1a6f6d4
Monkeytoes999 <graytokarz@yahoo.com>
2022-02-21 17:14:14 -0500

    Cookie inital test + positional widget creation

--- list.js
+++ list.js
@@ -12,8 +12,25 @@ function welcome() {
     welcome.update = function() {
     }
     widgets.push(new Case(welcome));
+    pos.push([]);
 }
-welcome();
+
+function posWelcome(x,y) {
+    var welcome = new Widget();
+    welcome.build = function() {
+        var head = document.createElement("h1");
+        head.innerHTML = "Welcome";
+        head.style.textAlign = "center";
+    
+        this.setBase(head);
+    }
+
+    welcome.type = "Welcome";
+    welcome.update = function() {
+    }
+    widgets.push(new Case(welcome));
+    pos.push([x, y]);
+}
 
 function clock() {
     var clock = new Widget();
@@ -30,5 +47,33 @@ function clock() {
         document.getElementById(this.id).innerHTML = new Date();
     }
     widgets.push(new Case(clock));
+    pos.push([]);
 }
+
+function posClock(x, y) {
+    var clock = new Widget();
+    clock.build = function() {
+        var main = document.createElement("p");
+        main.style.textAlign = "center";
+        main.innerHTML = new Date();
+        main.id = this.id;
+    
+        this.setBase(main);
+    }
+    clock.type = "Clock";
+    clock.update = function() {
+        document.getElementById(this.id).innerHTML = new Date();
+    }
+    widgets.push(new Case(clock));
+    pos.push([x,y]);
+}
+
+welcome();
 clock();
+clock();
+clock();
+clock();
+welcome();
+welcome();
+posWelcome(540,420);
+posClock(540,520);
