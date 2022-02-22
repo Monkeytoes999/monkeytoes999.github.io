3aedfa5c76b08f6b29931038da863a10d325beb1
Monkeytoes999 <graytokarz@yahoo.com>
2022-02-22 00:56:10 -0500

    I don't know how your code works hopefully this ok

--- cookieStart.js
+++ cookieStart.js
@@ -1,4 +1,17 @@
 function save() {
-    // Put the cookie saving data here.
-    // I'll call save ocasionally. (Expert speller)
+    tC = "types=";
+    xC = "xpos=";
+    yc = "ypos=";
+    cookies = document.cookie.split("; ");
+    for (let i = 0; i<types.length; i++){
+        tc = tc + types[i] + ",";
+        xc = xc + positions[i][0] + ",";
+        yc = yc + positions[i][1] + ",";
+    }
+    tc = tc.substring(0, tc.length-1);
+    xc = xc.substring(0, xc.length-1);
+    yc = yc.substring(0, yc.length-1);    
+    document.cookie = tc + "; expires=Thu, 7 Dec 2056 12:00:00 UTC;"
+    document.cookie = xc + "; expires=Thu, 7 Dec 2056 12:00:00 UTC;"
+    document.cookie = yc + "; expires=Thu, 7 Dec 2056 12:00:00 UTC;"
 }
--- initialize.js
+++ initialize.js
@@ -3,3 +3,17 @@ var globalX = 0;
 var globalY = 0;
 var types = [];
 var positions = [];
+if (document.cookies.includes("types=")){
+    cookies = document.cookie.split("; ");
+    let i = 0;
+    for (i = 0; !cookies[i].includes("types="); i++);
+    ta = cookies[i].substring(6).split(',');
+    for (i = 0; !cookies[i].includes("xpos="); i++);
+    xa = cookies[i].substring(5).split(',');
+    for (i = 0; !cookies[i].includes("ypos="); i++);
+    ya = cookies[i].substring(5).split(',');
+    for (i = 0; i < ta.length; i++){
+        types.push(ta[i]);
+        positions.push([xa[i], ya[i]]);
+    }
+}
