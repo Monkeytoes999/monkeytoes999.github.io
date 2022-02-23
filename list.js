7a800b0788b0c20a96ed75792133c89bc038d05d
Monkeytoes999 <graytokarz@yahoo.com>
2022-02-23 16:03:21 -0500

    Dad Joke. And a way to stop the async curse.

--- list.js
+++ list.js
@@ -16,3 +16,16 @@ creators.push(function() {
     }
     return new Case(welcome);
 });
+creators.push(function() {
+    var dadjoke = new Widget();
+    dadjoke.build = function() {
+        var head = document.createElement("h1");
+        fetch("https://icanhazdadjoke.com/slack").then(data => data.json().then(d => head.innerHTML = (d["attachments"]["0"]["text"])))        
+        head.style.textAlign = "center";
+        head.style.fontSize = "18px";
+        this.setBase(head);
+    }
+    dadjoke.name = "Dad Joke";
+    dadjoke.update = function() {}
+    return new Case(dadjoke);
+});
