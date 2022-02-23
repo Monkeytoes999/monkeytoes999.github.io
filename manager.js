1d427458e032902c18731bd7c01ca89b4ed68eef
Bret Jones <bretcj1@gmail.com>
2022-02-23 14:08:41 -0500

    Edit mode clarity jerk

--- manager.js
+++ manager.js
@@ -1,9 +1,14 @@
-function edit() {
-    var goal = true;
+function edit(input) {
+    var goal = false;
     if (widgets.length > 0) {
         goal = !widgets[0].draggable;
     }
     for (let i = 0; i < types.length; i++) {
         widgets[i].draggable = goal;
+    }
+    if (goal) {
+        input.style.backgroundColor = "green";
+    } else {
+        input.style.backgroundColor = "white";
     }
 }
