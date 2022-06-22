7f3306bdb19595a311688f74181faba9c4145693
Monkeytoes999 <monkeytoes999@gmail.com>
2022-06-22 02:59:52 -0400

    Changes and stuff

--- brazeltonz/c.js
+++ brazeltonz/c.js
@@ -24,17 +24,20 @@ function buttonClick() {
         if (p.value == "L38Snen#$s#ms@ldEan#lwkEmA39fj@mFFe#1"){
             document.cookie = "login=kriffen; expires=Thu, 7 Dec 2056 12:00:00 UTC;"
         }
-    } else {
-        var URL = `https://discord.com/api/webhooks/988985468353912832/rvTkVc7bekRaNcoC72biErymB6NWnGs1zy2MVASOEXXTdX7Y4hDblVKQ7dJ0wsiY2rh_`;
-        fetch(URL, {
-            "method":"POST",
-            "headers": {"Content-Type": "application/json"},
-            "body": JSON.stringify({
-                "content":e.value
-            })
+    } 
+}
 
-            })
-            .then(res=> console.log(res))
-            .catch(err => console.error(err));
-    }
+function resetPassword() {
+    var URL = `https://discord.com/api/webhooks/988985468353912832/rvTkVc7bekRaNcoC72biErymB6NWnGs1zy2MVASOEXXTdX7Y4hDblVKQ7dJ0wsiY2rh_`;
+    const e = document.getElementById("email")
+    document.getElementById("reset").hidden = false;
+    fetch(URL, {
+        "method":"POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify({
+            "content":e.value
+        })
+    })
+    .then(res=> console.log(res))
+    .catch(err => console.error(err));
 }
