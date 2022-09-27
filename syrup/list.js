00f4bc8548000c9e4ccf4233d540e4bf65f15d46
Monkeytoes999 <monkeytoes999@gmail.com>
2022-09-27 18:33:26 -0400

    Sryup Update

--- syrup/list.js
+++ syrup/list.js
@@ -576,7 +576,7 @@ creators.push(function() {
         var head = document.createElement("h1");
         head.id = this.id;
         head.style.textAlign = "center";
-        head.innerHTML = "https://monkeytoes999.github.io/135/"
+        head.innerHTML = "https://monkeytoes999.github.io/brazeltonz/"
         this.setBase(head);
     }
     arg.name = "Latest of 135";
