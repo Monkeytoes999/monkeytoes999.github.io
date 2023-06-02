aef70a4ec61da7902d284f0637963980140ce272
Monkeytoes999 <monkeytoes999@gmail.com>
2023-06-02 12:31:01 -0400

    :D3:

--- fof/franse.js
+++ fof/franse.js
@@ -57,3 +57,68 @@ async function D2S() {
         });
     }
 }
+
+function DayThree() {
+    document.getElementById("wrp").hidden = true;
+    document.getElementById("d3").hidden = false;
+}
+
+function D3C1() {
+    if (document.getElementById('C1').innerHTML == 34) {
+        document.getElementById('B1').disabled = true;
+        document.getElementById('C2').hidden = false;
+        document.getElementById('B2').hidden = false;
+        document.getElementById('B22').hidden = false;
+    }
+    document.getElementById('C1').innerHTML = parseInt(document.getElementById('C1').innerHTML) + 1;
+}
+
+function D3C2() {
+    if (document.getElementById('C2').innerHTML == 34) {
+        document.getElementById('B2').disabled = true;
+        document.getElementById('B22').disabled = true;
+        document.getElementById('C3').hidden = false;
+        document.getElementById('B3').hidden = false;
+        document.getElementById('B32').hidden = false;
+        document.getElementById('B33').hidden = false;
+    }
+    document.getElementById('C2').innerHTML = parseInt(document.getElementById('C2').innerHTML) + 1;
+}
+
+function D3C22() {
+    window.location.href = "/fof/D3VS/0293.mp4";
+}
+
+function D3C3() {
+    window.location.href = "/fof/D3VS/8263.mp4"
+}
+
+function D3C32() {
+    window.location.href = "/fof/D3VS/insertnumber.mp4"
+}
+
+function D3C33() {
+    if (document.getElementById('C3').innerHTML == 34) {
+        document.getElementById('B3').disabled = true;
+        document.getElementById('B32').disabled = true;
+        document.getElementById('B33').disabled = true;
+        document.getElementById('MSI').hidden = false;
+        document.getElementById('MSB').hidden = false;
+    }
+    document.getElementById('C3').innerHTML = parseInt(document.getElementById('C3').innerHTML) + 1;
+}
+
+async function D3S() {
+    field = document.getElementById('MSI').value
+    if (field != undefined && field.length > 0){
+        await fetch("https://mhrduality.vercel.app/p5/d2/" + field).then(data => {
+            data.json().then(response => {
+                r = response["r"];
+                if (r == "Y"){
+                    document.getElementById('D3E').src = response["f"];
+                    document.getElementById('D3E').hidden = false;
+                }
+            });
+        });
+    }
+}
