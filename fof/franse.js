a87ea25dc5b92ec556f9a539fa26095a36141970
Monkeytoes999 <monkeytoes999@gmail.com>
2023-06-05 14:57:26 -0400

    d5

--- fof/franse.js
+++ fof/franse.js
@@ -157,3 +157,294 @@ async function D4S() {
         });
     }
 }
+
+function home() {
+    document.getElementById("d5").hidden = true;
+    document.getElementById("wrp").hidden = false;
+}
+
+function DayFive() {
+    document.getElementById("wrp").hidden = true;
+    document.getElementById("d5").hidden = false;
+}
+
+throwing = false;
+numord = 0;
+haveBq = false;
+haveBag = false;
+tut = 0;
+function ord() {
+    document.getElementById('tbF').hidden = false;
+    if (numord == 0){
+        setTimeout(() => {
+            document.getElementById('tabs').hidden = false;
+            alert("Oh no! This native french who only speaks the way French usually do is hungry and made the mistake of assuming your bakery would serve fine fancy French food! Quick! Switch to the fooooooods tab and make this guy a traditional French deliciously, the Båguéttè")
+        }, 500);
+    }
+    numord += 1
+}
+
+function Orders() {
+    document.getElementById("kit").hidden = true;
+    document.getElementById("bko").hidden = false;
+    if (haveBq) {
+        haveBq = false;
+        bqTime();
+    } else if(haveBag) {
+        haveBag = false;
+        bagTime();
+    }
+}
+
+function Food() {
+    document.getElementById("kit").hidden = false;
+    document.getElementById("bko").hidden = true;
+    if (tut == 1){
+        document.getElementById("bq").hidden = true;
+        setTimeout(() => {
+            alert("Uh oh, it seems you threw all the båguéttès in the trash can by accident! You can't feed those to people! And the box of båguéttès you thought you bought yesterday was actually just a bunch of wet bags! Guess you'll just have to pass them off as bagwets and hope they don't notice.")
+        }, 50);
+    }
+}
+
+function ClBq() {
+    document.getElementById("bqGrab").hidden = true;
+    document.getElementById("bqBUT").hidden = true;
+    document.getElementById("bqFeed").hidden = false;
+    haveBq = true;
+    setTimeout(() => {
+        alert("Nice! Now let's go feed that hungry guy from the world of Fräns!");
+    }, 500);
+}
+
+l = 41.8;
+t = 57.2;
+rot = 0;
+goall = 43.8;
+goalt = 50.2;
+function bqTime() {
+    setTimeout(() => {
+        if (Math.random() > .95) {
+            l = l + .2;
+            if (l > goalt) {
+                l = goall
+            }
+        }
+        if (Math.random() > .9) {
+            t = t - .2;
+            if (t < goalt) {
+                t = goalt
+            }
+        }
+        rot += Math.random()*8
+        document.getElementById("bqFeed").style.left = l + "vw"
+        document.getElementById("bqFeed").style.top = t + "vh"
+        document.getElementById("bqFeed").style.transform = "rotate(" + rot + "deg)"
+        if (l != goall && t != goalt) {
+            bqTime();
+        } else {
+            document.getElementById("tbF").hidden = true;
+            document.getElementById("cusY").hidden = false;
+            setTimeout(() => {
+                retreat();
+            }, 1000);
+        }
+    }, 10)
+}
+
+function retreat() {
+    document.getElementById("cusY").hidden = true;
+    document.getElementById("cp3txt").hidden = true;
+    document.getElementById("ouch").hidden = true;
+    for (i = 0; i < 150; i++){
+        setTimeout(() => {
+            document.getElementById("bd").style.top = parseFloat(document.getElementById("bd").style.top.substring(0, 4)) - -.1 + "vh";
+            document.getElementById("hd").style.bottom = parseFloat(document.getElementById("hd").style.bottom.substring(0, 4)) - .1 + "vh";
+            document.getElementById("ar1").style.top = parseFloat(document.getElementById("ar1").style.top.substring(0, 4)) - -.1 + "vh";
+            document.getElementById("ar2").style.top = parseFloat(document.getElementById("ar2").style.top.substring(0, 4)) - -.1 + "vh";
+            document.getElementById("bqFeed").style.top = parseFloat(document.getElementById("bqFeed").style.top.substring(0, 4)) - -.1 + "vh";
+            document.getElementById("bagFeed").style.top = parseFloat(document.getElementById("bagFeed").style.top.substring(0,4)) - -.1 + "vh";
+        }, i*10);
+    }
+    if (tut == 0) {
+        setTimeout(() => {
+            document.getElementById("bqFeed").hidden = true;
+            tut = 1;
+            alert("Oh no! That guy told all his friends about us and now we're going to have to deal with a bunch of customers!");
+            document.getElementById("bw").hidden = false;
+            emerge()
+        }, 2000);
+    } else if (tut == 1) {
+        setTimeout(() => {
+            document.getElementById("bagFeed").hidden = true;
+            tut = 2;
+            alert("Whoo! They fell for it. How long can we keep this up?")
+            document.getElementById("counter").hidden = false;
+            document.getElementById("counter").innerHTML = "Customers served: 2"
+            emerge();
+        }, 2000);
+    } else if (tut == 5) {
+        setTimeout(() => {
+            document.getElementById("delivery").hidden = true;
+            tut = tut + 1
+            document.getElementById("counter").innerHTML = "Customers served: " + tut;
+            alert("You've unlocked fast mode! You know the number you need to get to at this point, so here's a way to skip the animation!");
+            emerge();
+        }, 2000);
+    } else if (tut == 35) {
+        setTimeout(() => {
+            document.getElementById("day").hidden = true;
+            tut = tut + 1
+            document.getElementById("counter").innerHTML = "Customers served: " + tut;
+            emerge();
+        }, 2000);
+    } else {
+        setTimeout(() => {
+            document.getElementById("bagFeed").hidden = true;
+            tut = tut + 1
+            document.getElementById("counter").innerHTML = "Customers served: " + tut;
+            emerge();
+        }, 2500);
+    }
+}
+
+hCount = 3
+function emerge() {
+    for (i = 0; i < 150; i++){
+        setTimeout(() => {
+            document.getElementById("bd").style.top = parseFloat(document.getElementById("bd").style.top.substring(0, 4)) - .1 + "vh";
+            document.getElementById("hd").style.bottom = parseFloat(document.getElementById("hd").style.bottom.substring(0, 4)) - -.1 + "vh";
+            document.getElementById("ar1").style.top = parseFloat(document.getElementById("ar1").style.top.substring(0, 4)) - .1 + "vh";
+            document.getElementById("ar2").style.top = parseFloat(document.getElementById("ar2").style.top.substring(0, 4)) - .1 + "vh";
+            document.getElementById("bqFeed").style.top = parseFloat(document.getElementById("bqFeed").style.top.substring(0, 4)) - .1 + "vh";
+            document.getElementById("bagFeed").style.top = parseFloat(document.getElementById("bagFeed").style.top.substring(0,4)) - .1 + "vh";
+        }, i*10);
+    }
+    document.getElementById("bagFeed").style.left = "39.8vw"
+    document.getElementById("bagFeed").style.top = "55.8vh"
+    document.getElementById("bagFeed").style.transform = "rotate(0deg)"
+    setTimeout(() => {
+        document.getElementById("cp3txt").hidden = false;
+        if (hCount == 3){
+            document.getElementById("bw1").hidden = false;
+            document.getElementById("bw1BUT").hidden = false;
+            document.getElementById("bw2").hidden = false;
+            document.getElementById("bw2BUT").hidden = false;
+            document.getElementById("bw3").hidden = false;
+            document.getElementById("bw3BUT").hidden = false;    
+            hCount = 0
+        }
+    }, 2000)
+}
+
+function BW1() {
+    if (!haveBag) {
+        document.getElementById("bw1").hidden = true;
+        document.getElementById("bw1BUT").hidden = true;
+        document.getElementById("bagFeed").hidden = false;
+        hCount = hCount + 1;
+    }
+    haveBag = true;
+    l = 39.8;
+    t = 55.8;
+    rot = 0;
+    goall = 45.6;
+    goalt = 50.2;
+}
+
+function BW2() {
+    if (!haveBag) {
+        document.getElementById("bw2").hidden = true;
+        document.getElementById("bw2BUT").hidden = true;
+        document.getElementById("bagFeed").hidden = false;
+        hCount = hCount + 1;
+    }
+    haveBag = true;
+    l = 39.8;
+    t = 55.8;
+    rot = 0;
+    goall = 45.6;
+    goalt = 50.2;
+}
+
+function BW3() {
+    if (!haveBag) {
+        document.getElementById("bw3").hidden = true;
+        document.getElementById("bw3BUT").hidden = true;
+        document.getElementById("bagFeed").hidden = false;
+        hCount = hCount + 1;
+    }
+    haveBag = true;
+    l = 39.8;
+    t = 55.8;
+    rot = 0;
+    goall = 45.6;
+    goalt = 50.2;
+}
+
+function bagTime() {
+    if (!throwing) {
+        setTimeout(() => {
+            if (Math.random() > .95) {
+                l = l + .2;
+                if (l > goall) {
+                    l = goall;
+                }
+            }
+            if (Math.random() > .9) {
+                t = t - .2;
+                if (t < goalt) {
+                    t = goalt;
+                }
+            }
+            rot += Math.random()*8;
+            document.getElementById("bagFeed").style.left = l + "vw";
+            document.getElementById("bagFeed").style.top = t + "vh";
+            document.getElementById("bagFeed").style.transform = "rotate(" + rot + "deg)";
+            if (l != goall || t != goalt) {
+                bagTime();
+            } else {
+                document.getElementById("tbF").hidden = true;
+                document.getElementById("cusY").hidden = false;
+                setTimeout(() => {
+                    retreat();
+                }, 1000);
+            }
+        }, 10)
+    } else {
+        document.getElementById("bagFeed").style.left = "47.5vw";
+        document.getElementById("bagFeed").style.top = "46.5vh";
+        document.getElementById("bagFeed").style.top = "rotate(" + Math.random()*360 + "deg)";
+        document.getElementById("tbF").hidden = true;
+        document.getElementById("ouch").hidden = false;
+        setTimeout(() => {
+            retreat();
+        }, 300);
+    }
+    
+}
+
+function Delivery() {
+    if (throwing) {
+        throwing = false;
+        document.getElementById("delivery").innerHTML = "Delivery Mode: Handing";
+    } else {
+        throwing = true;
+        document.getElementById("delivery").innerHTML = "Delivery Mode: Throwing";
+    }
+}
+
+async function D5T() {
+    field = document.getElementById('bred').value
+    if (field != undefined && field.length > 0){
+        await fetch("https://mhrduality.vercel.app/p5/d4/" + field).then(data => {
+            data.json().then(response => {
+                r = response["r"];
+                if (r == "FRN"){
+                    document.getElementById('DE5').src = response["f"];
+                    document.getElementById('DE5').style.visibility = "visible";
+                }
+            });
+        });
+    }
+}
