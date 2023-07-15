ecb8c224c857866e26eae5049d816b6d78e9907f
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2023-07-15 00:12:53 -0400

    Jerraldi reaction incoming

--- lvs/lvstr.js
+++ lvs/lvstr.js
@@ -1,11 +1,19 @@
-function lobsterOn() {
-    fetch('https://mhrduality.vercel.app/lv/lbc1');
-}
-
-function lobsterOff() {
-    fetch('https://mhrduality.vercel.app/lv/lbc0');
+function lobsterToggle() {
+    var base = document.getElementById("lobster");
+    var lobster = base.classList.contains("active");
+    if (lobster) {
+        fetch('https://mhrduality.vercel.app/lv/lbc0');
+    } else {
+        fetch('https://mhrduality.vercel.app/lv/lbc1');
+    }
+    lobster = !lobster;
+    base.classList.toggle("active");
 }
 
 function tasty() {
     fetch('https://mhrduality.vercel.app/lv/whtasty');
+}
+
+function awYeah() {
+    fetch('https://mhrduality.vercel.app/lv/whawyeah');
 }
