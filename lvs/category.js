c218f1f841260199d6d7a52d43e018d32dfd3889
Bret Jones <45405964+Iceman013@users.noreply.github.com>
2023-07-31 21:29:50 -0400

    Aww yeah

--- lvs/category.js
+++ lvs/category.js
@@ -3,6 +3,16 @@ function Category(name, color) {
     this.color = color;
 
     this.element = document.createElement("div");
+    this.element.onclick = function (e) {
+        var cow = this;
+        if (cow.style.width == "fit-content") {
+            cow.style.height = "44px";
+            cow.style.width = "104px";
+        } else {
+            cow.style.height = "fit-content";
+            cow.style.width = "fit-content";
+        }
+    }
     this.element.classList.add("category");
 
     this.element.id = this.name;
