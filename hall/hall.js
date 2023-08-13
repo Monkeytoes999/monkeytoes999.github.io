94f2ad76a6248ebeb1553b3fe62ae8b4d38c5fb3
Monkeytoes999 <monkeytoes999@gmail.com>
2023-08-13 15:00:24 -0400

    Hall - 113

--- hall/hall.js
+++ hall/hall.js
@@ -10,14 +10,15 @@ function home() {
 }
 
 ibNum = 1;
-totAdded = 2;
+totAdded = 3;
 ibNms = [111, 112, 113, 114, 212, 213, 311, 312, 411, 413];
 //SE, 2/17
 ibNas = ["TMBG", "Straws", "Pencils", "Swedish", "Calculators", "Cubing", "Temperature", "Eating Paper", "Pockets", "Peanut Butter"];
-ibRp = ["NA", "NA"];
+ibRp = ["NA", "NA", "NA"];
 ibShD = ["They Might Be Giants - an alt rock band that I grew up listening to quite frequently through my father. I've been to two live shows and they're the main source of my musical taste, with their tracks taking up over 75% of my personal music library last I checked.",
-"Straws are an unreasonably high percentage of aquatic litter for being a useless tube. It's not even the turtle thing, it's bigger than that. There's no real reason for us to use straws as much as we do, at least not plastic ones. Use glass if you need to. I've written school reports on straws at least once a year since 10th."];
-ibVd = ["https://youtu.be/dW7y-Ao4mSg", "https://youtu.be/hZ9asyBt-04"];
+"Straws are an unreasonably high percentage of aquatic litter for being a useless tube. It's not even the turtle thing, it's bigger than that. There's no real reason for us to use straws as much as we do, at least not plastic ones. Use glass if you need to. I've written school reports on straws at least once a year since 10th.",
+"I've been collecting writing utensils for a long time at this point. Any time I see a pen/pencil/whatever that somebody has left on the floor or lost or whatever I will add it to my collection of pocket pencil, which eventually get transferred into the shoebox. I usually have at least one of them on me at any given time, which we'll discuss in a bit with the whole pockets thing. I appreciate pencils more than pens, even though pens are winning."];
+ibVd = ["https://youtu.be/dW7y-Ao4mSg", "https://youtu.be/hZ9asyBt-04", "https://youtu.be/DKtV2JtViEs"];
 
 function hall() {
     document.getElementById("mnWcl").hidden = true;
