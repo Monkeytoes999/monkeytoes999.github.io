474440ef93abe1cc79a6a53ed2c429e0221d7711
Graham Tokarz <monkeytoes999@gmail.com>
2025-08-30 22:03:11 -0400

    LVS update bc that makes sense

--- lvs/lvstr.js
+++ lvs/lvstr.js
@@ -2,9 +2,9 @@ function lobsterToggle() {
     var base = document.getElementById("lobster");
     var lobster = false; //base.classList.contains("active");
     if (lobster) {
-        fetch('https://mhrdualityu.vercel.app/lv/lbc0');
+        fetch('https://mhrduality.vercel.app/lv/lbc0');
     } else {
-        fetch('https://mhrdualityu.vercel.app/lv/lbc1');
+        fetch('https://mhrduality.vercel.app/lv/lbc1');
     }
     //lobster = !lobster;
     //base.classList.toggle("active");
@@ -20,25 +20,25 @@ const SOUND_CATS = [
     new Category("Rate Limited", "#eb9e34"),
 ];
 const BUTTONS = [
-    new Button("Tasty", "sound", "https://mhrdualityu.vercel.app/lv/whtasty", ["Key Phrases", "Video Games"]),
-    new Button("Reloading", "sound", "https://mhrdualityu.vercel.app/lv/whreloading", ["Key Phrases", "Video Games"]),
-    new Button("Kostimo", "sound", "https://mhrdualityu.vercel.app/lv/whkostimo", ["RNG Groups"]),
-    new Button("Aw Yeah", "sound", "https://mhrdualityu.vercel.app/lv/whawyeah", ["Key Phrases", "Music"]),
-    new Button("Discord Ping", "sound", "https://mhrdualityu.vercel.app/lv/whdiscord", ["Sound"]),
-    new Button("Lights Off", "sound", "https://mhrdualityu.vercel.app/lv/whlights", ["Music"]),
-    new Button("Hurt Garm", "sound", "https://mhrdualityu.vercel.app/lv/whtorture", ["Long Stuff", "Rate Limited"]),
-    new Button("You Clod", "sound", "https://mhrdualityu.vercel.app/lv/whclod", ["TV Shows"]),
-    new Button("Cooking", "sound", "https://mhrdualityu.vercel.app/lv/whcooking", ["TV Shows", "Long Stuff"]),
-    new Button("Owl House", "sound", "https://mhrdualityu.vercel.app/lv/whowlh", ["TV Shows"]),
-    new Button("Pew pew", "sound", "https://mhrdualityu.vercel.app/lv/whpew", ["Video Games", "RNG Groups"]),
-    new Button("Fingertips", "sound", "https://mhrdualityu.vercel.app/lv/whfingertips", ["Music"]),
-    new Button("Portal 2 SFX", "sound", "https://mhrdualityu.vercel.app/lv/whp2sfx", ["Video Games", "RNG Groups"]),
-    new Button("Hardly", "sound", "https://mhrdualityu.vercel.app/lv/whhkh", ["Key Phrases"])
+    new Button("Tasty", "sound", "https://mhrduality.vercel.app/lv/whtasty", ["Key Phrases", "Video Games"]),
+    new Button("Reloading", "sound", "https://mhrduality.vercel.app/lv/whreloading", ["Key Phrases", "Video Games"]),
+    new Button("Kostimo", "sound", "https://mhrduality.vercel.app/lv/whkostimo", ["RNG Groups"]),
+    new Button("Aw Yeah", "sound", "https://mhrduality.vercel.app/lv/whawyeah", ["Key Phrases", "Music"]),
+    new Button("Discord Ping", "sound", "https://mhrduality.vercel.app/lv/whdiscord", ["Sound"]),
+    new Button("Lights Off", "sound", "https://mhrduality.vercel.app/lv/whlights", ["Music"]),
+    new Button("Hurt Garm", "sound", "https://mhrduality.vercel.app/lv/whtorture", ["Long Stuff", "Rate Limited"]),
+    new Button("You Clod", "sound", "https://mhrduality.vercel.app/lv/whclod", ["TV Shows"]),
+    new Button("Cooking", "sound", "https://mhrduality.vercel.app/lv/whcooking", ["TV Shows", "Long Stuff"]),
+    new Button("Owl House", "sound", "https://mhrduality.vercel.app/lv/whowlh", ["TV Shows"]),
+    new Button("Pew pew", "sound", "https://mhrduality.vercel.app/lv/whpew", ["Video Games", "RNG Groups"]),
+    new Button("Fingertips", "sound", "https://mhrduality.vercel.app/lv/whfingertips", ["Music"]),
+    new Button("Portal 2 SFX", "sound", "https://mhrduality.vercel.app/lv/whp2sfx", ["Video Games", "RNG Groups"]),
+    new Button("Hardly", "sound", "https://mhrduality.vercel.app/lv/whhkh", ["Key Phrases"])
 ];
 
 // makeCaptcha(); // Figure out a way to disable for just me
 const TIMETOCHECK = 60; // Minutes between captchas
-fetch("https://mhrdualityu.vercel.app/lv/src")
+fetch("https://mhrduality.vercel.app/lv/src")
         .then(data => data.json()
         .then(d => document.getElementById("VIDEO").src = d["v"]));
 setInterval(makeCaptcha, 60*1000*TIMETOCHECK);
